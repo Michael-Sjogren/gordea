70a2cbac95b59cc7fc773820dbaf882da280fb50
Michael <Michael-Sjogren>
2023-07-16 14:22:58 +0200

    add mux as dependecy

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello")
+
+	addr := "localhost:8083"
+
+	http.ListenAndServe(addr, nil)
+}
+
+func handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
